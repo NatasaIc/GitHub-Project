6822e41c7111ab6a9cda6d885a654eedb1e60539
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2022-11-28 13:15:50 +0100

    API

---
+++ src/ts/main.ts
@@ -0,0 +1,25 @@
+import { IMovie } from "./models/IMovie";
+import { IOmdbResponse } from "./models/IOmdbResponse";
+import axios from "axios";
+
+axios
+  .get<IOmdbResponse>("http://www.omdbapi.com/?apikey=416ed51a&s=Pulp")
+  .then((response) => {
+    console.log(response.data.Search);
+    createHtml(response.data.Search);
+  });
+
+function createHtml(movies: IMovie[]): void {
+  let container: HTMLDivElement = document.getElementById(
+    "movieContainer"
+  ) as HTMLDivElement;
+
+  for (let i: number = 0; i < movies.length; i++) {
+    console.log(movies[i].Title);
+
+    let title: HTMLHeadingElement = document.createElement("h3");
+    title.innerHTML = movies[i].Title;
+
+    container.appendChild(title);
+  }
+}
